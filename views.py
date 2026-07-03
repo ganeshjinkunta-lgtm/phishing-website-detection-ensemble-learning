de88c1b0795214aa9aa225782110af8140876c39
ganeshjinkunta-lgtm <ganeshjinkunta@gmail.com>
2026-07-03 18:36:08 +0530

    Add files via upload

---
+++ views.py
@@ -0,0 +1,184 @@
+from django.db.models import Count
+from django.db.models import Q
+from django.shortcuts import render, redirect, get_object_or_404
+
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+from sklearn.metrics import accuracy_score
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import VotingClassifier
+# Create your views here.
+from Remote_User.models import ClientRegister_Model,detect_phishing_websites,detection_ratio,detection_accuracy
+
+def login(request):
+
+
+    if request.method == "POST" and 'submit1' in request.POST:
+
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        try:
+            enter = ClientRegister_Model.objects.get(username=username,password=password)
+            request.session["userid"] = enter.id
+
+            return redirect('ViewYourProfile')
+        except:
+            pass
+
+    return render(request,'RUser/login.html')
+
+def index(request):
+    return render(request, 'RUser/index.html')
+
+def Add_DataSet_Details(request):
+
+    return render(request, 'RUser/Add_DataSet_Details.html', {"excel_data": ''})
+
+
+def Register1(request):
+
+    if request.method == "POST":
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        phoneno = request.POST.get('phoneno')
+        country = request.POST.get('country')
+        state = request.POST.get('state')
+        city = request.POST.get('city')
+        address = request.POST.get('address')
+        gender = request.POST.get('gender')
+        ClientRegister_Model.objects.create(username=username, email=email, password=password, phoneno=phoneno,
+                                            country=country, state=state, city=city,address=address,gender=gender)
+
+        obj = "Registered Successfully"
+        return render(request, 'RUser/Register1.html',{'object':obj})
+    else:
+        return render(request,'RUser/Register1.html')
+
+def ViewYourProfile(request):
+    userid = request.session['userid']
+    obj = ClientRegister_Model.objects.get(id= userid)
+    return render(request,'RUser/ViewYourProfile.html',{'object':obj})
+
+
+def Predict_Website_Type(request):
+    if request.method == "POST":
+
+        if request.method == "POST":
+
+            website_name= request.POST.get('website_name')
+
+        df = pd.read_csv('Datasets.csv', encoding='latin-1')
+
+        def apply_results(label):
+            if (label == 0):
+                return 0  # Normal Website
+            elif (label == 1):
+                return 1  # Phishing Website
+
+        df['results'] = df['Label'].apply(apply_results)
+
+        cv = CountVectorizer(lowercase=False, strip_accents='unicode', ngram_range=(1, 1))
+
+        X = df['website_name']
+        y = df['results']
+
+        print("Website Name")
+        print(X)
+        print("Results")
+        print(y)
+
+        X = cv.fit_transform(X)
+
+        models = []
+        from sklearn.model_selection import train_test_split
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20)
+        X_train.shape, X_test.shape, y_train.shape
+
+        print("Gradient Boosting Classifier")
+
+        from sklearn.ensemble import GradientBoostingClassifier
+        clf = GradientBoostingClassifier(n_estimators=100, learning_rate=1.0, max_depth=1, random_state=0).fit(
+            X_train,
+            y_train)
+        clfpredict = clf.predict(X_test)
+        print("ACCURACY")
+        print(accuracy_score(y_test, clfpredict) * 100)
+        print("CLASSIFICATION REPORT")
+        print(classification_report(y_test, clfpredict))
+        print("CONFUSION MATRIX")
+        print(confusion_matrix(y_test, clfpredict))
+        models.append(('GradientBoostingClassifier', clf))
+
+
+        # SVM Model
+        print("SVM")
+        from sklearn import svm
+
+        lin_clf = svm.LinearSVC()
+        lin_clf.fit(X_train, y_train)
+        predict_svm = lin_clf.predict(X_test)
+        svm_acc = accuracy_score(y_test, predict_svm) * 100
+        print("ACCURACY")
+        print(svm_acc)
+        print("CLASSIFICATION REPORT")
+        print(classification_report(y_test, predict_svm))
+        print("CONFUSION MATRIX")
+        print(confusion_matrix(y_test, predict_svm))
+        models.append(('svm', lin_clf))
+
+        print("Logistic Regression")
+
+        from sklearn.linear_model import LogisticRegression
+
+        reg = LogisticRegression(random_state=0, solver='lbfgs').fit(X_train, y_train)
+        y_pred = reg.predict(X_test)
+        print("ACCURACY")
+        print(accuracy_score(y_test, y_pred) * 100)
+        print("CLASSIFICATION REPORT")
+        print(classification_report(y_test, y_pred))
+        print("CONFUSION MATRIX")
+        print(confusion_matrix(y_test, y_pred))
+        models.append(('logistic', reg))
+
+        print("Decision Tree Classifier")
+        dtc = DecisionTreeClassifier()
+        dtc.fit(X_train, y_train)
+        dtcpredict = dtc.predict(X_test)
+        print("ACCURACY")
+        print(accuracy_score(y_test, dtcpredict) * 100)
+        print("CLASSIFICATION REPORT")
+        print(classification_report(y_test, dtcpredict))
+        print("CONFUSION MATRIX")
+        print(confusion_matrix(y_test, dtcpredict))
+        models.append(('DecisionTreeClassifier', dtc))
+
+        classifier = VotingClassifier(models)
+        classifier.fit(X_train, y_train)
+        y_pred = classifier.predict(X_test)
+
+        website_name1 = [website_name]
+        vector1 = cv.transform(website_name1).toarray()
+        predict_text = classifier.predict(vector1)
+
+        pred = str(predict_text).replace("[", "")
+        pred1 = pred.replace("]", "")
+
+        prediction = int(pred1)
+
+        if (prediction == 0):
+            val = 'Normal Website'
+        elif (prediction == 1):
+            val = 'Phishing Website'
+
+        print(val)
+        print(pred1)
+
+        detect_phishing_websites.objects.create(website_name=website_name,Prediction=val)
+
+        return render(request, 'RUser/Predict_Website_Type.html',{'objs': val})
+    return render(request, 'RUser/Predict_Website_Type.html')
+
+
+
